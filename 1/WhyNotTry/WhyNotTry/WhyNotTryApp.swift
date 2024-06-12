22d914427c6511788848101172ed0055ca2982da
Asad <asad.mobiledev@gmail.com>
2024-06-12 16:58:23 +0500

    WhyNotTry Learn SwiftUI basics | Text, Image, Animation etc

---
+++ 1/WhyNotTry/WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Asad Mehmood on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
