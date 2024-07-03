b1b0a585aeecd64941de88422140435643efc39a
Asad <asad.mobiledev@gmail.com>
2024-07-03 20:09:26 +0500

    Lesson 3 Building Lists and Navigations

---
+++ 3/BuildingListsNNavigation/BuildingListsNNavigations/BuildingListsNNavigations/BuildingListsNNavigationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListsNNavigationsApp.swift
+//  BuildingListsNNavigations
+//
+//  Created by Asad Mehmood on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListsNNavigationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3/BuildingListsNNavigation/BuildingListsNNavigations/BuildingListsNNavigations/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  CreatingNCombiningViews
+//
+//  Created by Asad Mehmood on 16/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
